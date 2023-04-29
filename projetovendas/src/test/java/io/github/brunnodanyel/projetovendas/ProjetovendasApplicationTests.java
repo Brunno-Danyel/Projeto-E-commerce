f33d46012568623ac2a0025ccc931f5ab7fffc45
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-04-29 10:52:27 -0300

    Criação do projeto

---
+++ projetovendas/src/test/java/io/github/brunnodanyel/projetovendas/ProjetovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunnodanyel.projetovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
