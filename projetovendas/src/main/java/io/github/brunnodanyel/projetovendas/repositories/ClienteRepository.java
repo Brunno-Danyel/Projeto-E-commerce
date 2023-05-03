b0735ca3e8b79ace3674e02d8c5f0dd00702ca63
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-03 17:43:39 -0300

    Implementando o repositorio de cliente e suas buscas
    findByCpf: Realiza a busca pelo CPF do cliente
    findByEmail: Realiza a busca pelo Email do cliente

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/repositories/ClienteRepository.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/repositories/ClienteRepository.java
@@ -3,5 +3,11 @@ package io.github.brunnodanyel.projetovendas.repositories;
 import io.github.brunnodanyel.projetovendas.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<String, Cliente> {
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+   Optional <Cliente> findByCpf(String cpf);
+
+   Optional<Cliente> findByEmail(String email);
 }
