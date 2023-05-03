7fbd4e22ef052ae35432e5bea969b6ab57d2b8ee
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-03 17:38:08 -0300

    Ajustes na entidade Cliente

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Cliente.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Cliente.java
@@ -1,22 +1,29 @@
 package io.github.brunnodanyel.projetovendas.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class Cliente {
 
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "tb_cpf")
     private String cpf;
 
@@ -24,21 +31,26 @@ public class Cliente {
     private String nomeCompleto;
 
     @Column(name = "tb_data_nascimento")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
-    private Date dataNascimento;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy", locale = "pt-BR", timezone = "Brazil/East")
+    private LocalDate dataNascimento;
 
     @Column(name = "tb_telefone_celular")
-    private Integer telefoneCelular;
+    private String telefoneCelular;
 
     @Column(name = "tb_email")
     private String email;
 
+    @Column(name = "tb_senha")
+    private String senha;
+
     @Column(name = "tb_enderecos")
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
-    private List<Endereco> enderecos;
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.MERGE)
+    private List<Endereco> enderecos = new ArrayList<>();
 
     @Column(name = "tb_pedidos")
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Pedido> pedidos;
+
+    private boolean admin;
 
 }
