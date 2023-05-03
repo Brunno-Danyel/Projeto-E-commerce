ec6047a653cce73bfebfb454ff236be071904edf
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-03 17:44:50 -0300

    Implementando o endereço de cliente e suas buscas
    findByClienteCpf: busca pelo CPF do cliente que existi na entidade endereço

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/repositories/EnderecoRepository.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/repositories/EnderecoRepository.java
@@ -1,7 +1,10 @@
 package io.github.brunnodanyel.projetovendas.repositories;
-
 import io.github.brunnodanyel.projetovendas.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnderecoRepository extends JpaRepository<String, Endereco> {
+import java.util.List;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+    List<Endereco> findByClienteCpf(String cpf);
 }
