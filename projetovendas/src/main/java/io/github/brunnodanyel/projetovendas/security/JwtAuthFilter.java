68f1497c8e5ba6bec1da960cf2223d4525e9be07
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-04 10:24:06 -0300

    Implementando serviço de segurança para API com jwt

---
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/security/JwtAuthFilter.java
@@ -0,0 +1,48 @@
+package io.github.brunnodanyel.projetovendas.security;
+
+import io.github.brunnodanyel.projetovendas.services.ClienteService;
+import io.github.brunnodanyel.projetovendas.services.impl.ClienteServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+
+    private JwtService service;
+
+    private ClienteServiceImpl clienteService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = httpServletRequest.getHeader("Authorization");
+
+        if (authorization != null && authorization.startsWith("Bearer")) {
+            String token = authorization.split(" ")[1];
+            boolean isValid = service.tokenValido(token);
+
+            if (isValid) {
+                String loginUsuario = service.obterLogin(token);
+                UserDetails userDetails = clienteService.loadUserByUsername(loginUsuario);
+                UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                user.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(user);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+    }
+}
---
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package io.github.brunnodanyel.projetovendas.security;
+
+import io.github.brunnodanyel.projetovendas.services.impl.ClienteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClienteServiceImpl clienteService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+        return new JwtAuthFilter(jwtService, clienteService);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(clienteService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/clientes/buscar/cpf/").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/clientes/auth").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/clientes/cadastrar").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .and().headers().frameOptions().sameOrigin()
+                .and().authorizeRequests()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    response.setContentType("application/json");
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Token expirado: faça login novamente para obter um novo token!");
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setContentType("application/json");
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.getWriter().write("Acesso negado: Você não tem permissão para acessar esse serviço!");
+                });
+        ;
+    }
+}
