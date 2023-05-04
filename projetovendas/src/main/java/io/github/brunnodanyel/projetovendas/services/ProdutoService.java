4e8ff010ef7aefded729444745445a9853d2f5c4
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-04 19:34:21 -0300

    Criando os métodos da Interface de serviço do produto

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/services/ProdutoService.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/services/ProdutoService.java
@@ -3,10 +3,28 @@ package io.github.brunnodanyel.projetovendas.services;
 import io.github.brunnodanyel.projetovendas.model.dtoRequest.ProdutoAddRequestDTO;
 import io.github.brunnodanyel.projetovendas.model.dtoRequest.ProdutoRequestDTO;
 import io.github.brunnodanyel.projetovendas.model.dtoResponse.ProdutoResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProdutoService {
 
     void cadastrarProduto(ProdutoRequestDTO produtoRequestDTO);
 
-    ProdutoResponseDTO addProduto(Integer cod, ProdutoAddRequestDTO produtoAddRequestDTO);
+    ProdutoResponseDTO buscarCodigoDoProduto(String cod);
+
+    List<ProdutoResponseDTO> buscarMarcaProduto(String marca);
+
+    List<ProdutoResponseDTO> buscarNomeProduto(String nome);
+
+    List<ProdutoResponseDTO> buscarDescricaoProduto(String descricao);
+
+    List<ProdutoResponseDTO> buscarCategoriaProduto(String categoria);
+
+    List<ProdutoResponseDTO> buscarPrecoProduto(BigDecimal precoInicial, BigDecimal precoFinal);
+
+    List<ProdutoResponseDTO> listarTodos();
+
+    ProdutoResponseDTO addProduto(String cod, ProdutoAddRequestDTO produtoAddRequestDTO);
 }
