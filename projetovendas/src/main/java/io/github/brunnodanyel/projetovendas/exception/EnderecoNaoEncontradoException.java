d03d7dd1f8da69d1029d9c4ee554da34e129c707
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-06 06:59:36 -0300

    Criação de Classes exception personalizadas

---
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/exception/EnderecoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package io.github.brunnodanyel.projetovendas.exception;
+
+public class EnderecoNaoEncontradoException extends RuntimeException{
+
+    public EnderecoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
---
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/exception/SenhaInvalidaException.java
@@ -0,0 +1,8 @@
+package io.github.brunnodanyel.projetovendas.exception;
+
+public class SenhaInvalidaException extends RuntimeException{
+
+    public SenhaInvalidaException(String message) {
+        super(message);
+    }
+}
