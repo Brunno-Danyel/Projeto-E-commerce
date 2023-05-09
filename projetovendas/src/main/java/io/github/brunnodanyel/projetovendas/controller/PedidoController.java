3c782f61912b8cc8151b85c33140305169ec64a1
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-05-09 11:37:39 -0300

    Implementação do pedido Controller

---
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/controller/PedidoController.java
@@ -0,0 +1,23 @@
+package io.github.brunnodanyel.projetovendas.controller;
+
+import io.github.brunnodanyel.projetovendas.model.dtoRequest.PedidoRequestDTO;
+import io.github.brunnodanyel.projetovendas.model.dtoResponse.PedidoResponseDTO;
+import io.github.brunnodanyel.projetovendas.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/pedido")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @PostMapping("/realizarPedido")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PedidoResponseDTO realizarPedido(@RequestBody PedidoRequestDTO pedidoRequestDTO){
+        PedidoResponseDTO pedido = pedidoService.realizarPedido(pedidoRequestDTO);
+        return pedido;
+    }
+}
