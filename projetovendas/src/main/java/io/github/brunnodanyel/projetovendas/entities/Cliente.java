6f51c77ff53a1e9f2db69886c3abc70a425f9ec2
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2024-02-14 18:10:19 -0300

    Remoção da identificação das colunas

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Cliente.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Cliente.java
@@ -20,32 +20,25 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
     private Long id;
 
-    @Column(name = "tb_cpf")
     private String cpf;
 
-    @Column(name = "tb_nome_completo")
     private String nomeCompleto;
 
-    @Column(name = "tb_data_nascimento")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy", locale = "pt-BR", timezone = "Brazil/East")
     private LocalDate dataNascimento;
 
-    @Column(name = "tb_telefone_celular")
     private String telefoneCelular;
 
-    @Column(name = "tb_email")
     private String email;
 
-    @Column(name = "tb_senha")
     private String senha;
 
-    @Column(name = "tb_enderecos")
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.MERGE)
     private List<Endereco> enderecos = new ArrayList<>();
 
-    @Column(name = "tb_pedidos")
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Pedido> pedidos;
 
--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Endereco.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/entities/Endereco.java
@@ -16,33 +16,25 @@ public class Endereco {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco")
     private Long id;
 
-    @Column(name = "tb_cep")
     private String cep;
 
-    @Column(name = "tb_identificao")
     private String identificacao;
 
-    @Column(name = "tb_logradouro")
     private String logradouro;
 
-    @Column(name = "tb_numero")
     private String numero;
 
-    @Column(name = "tb_complemento")
     private String complemento;
 
-    @Column(name = "tb_referencia")
     private String referencia;
 
-    @Column(name = "tb_bairro")
     private String bairro;
 
-    @Column(name = "tb_cidade")
     private String localidade;
 
-    @Column(name = "tb_uf")
     private String uf;
 
     @ManyToOne
