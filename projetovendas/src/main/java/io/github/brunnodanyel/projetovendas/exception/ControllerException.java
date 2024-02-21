b3ccad5ca73e6304c3020bd19c5ce10de9326a41
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2024-02-21 18:20:57 -0300

    Melhoria

--- projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/exception/ControllerException.java
+++ projetovendas/src/main/java/io/github/brunnodanyel/projetovendas/exception/ControllerException.java
@@ -10,60 +10,62 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RestControllerAdvice
 public class ControllerException {
 
     @ExceptionHandler(EntidadeNaoEncontrada.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     public ApiErrors handleRegraNegocioException(EntidadeNaoEncontrada ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(ServicoCepException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ApiErrors handleRegraNegocioException(ServicoCepException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(EntidadeExistenteException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(EntidadeExistenteException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(BadRequestExecption.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(BadRequestExecption ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(IdadeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(IdadeException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(SenhaInvalidaException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(SenhaInvalidaException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(SenhaIncorretaException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseStatus(UNAUTHORIZED)
     public ApiErrors handleRegraNegocioException(SenhaIncorretaException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult().getAllErrors()
                 .stream()
